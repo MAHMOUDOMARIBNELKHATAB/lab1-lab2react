2d9dcc28c474bed0aeb7cf4db9df7e143724a301
MAHMOUDOMARIBNELKHATAB <momar351@yahoo.com>
2023-10-22 19:00:26 +0300

    redux task

--- src/components/counter-lab1/About.jsx
+++ src/components/counter-lab1/About.jsx
@@ -1,29 +1,40 @@
 import { useState } from "react";
 import './counter.css'
 import Button from 'react-bootstrap/Button';
+import { useSelector } from "react-redux";
+import store from "../../redux/store";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
+import { increment ,decrement } from "../../redux/slice/counterSlice";
+import { useDispatch } from "react-redux";
 
 
+
+ 
 const About = () => { 
-  const [counter, setCounter] = useState(10) 
-  
-  const handleClick1 = () => { 
-    setCounter(counter + 1) 
-  } 
-  
-  const handleClick2 = () => { 
-    setCounter(counter - 1) 
-  } 
+  const dispatch = useDispatch()
+
+  const counterGlobalState = useSelector(store => store.counter); 
+  console.log(counterGlobalState.counter);
+
+  const handelIncrement =() => {
+    dispatch(increment())
+
+  }
+  const handeldecrement =() => {
+    dispatch(decrement())
+
+  }
 
     return (
       <div  className="counter"> 
         Counter App 
         <div> 
-          {counter} 
+          {counterGlobalState.counter} 
         </div> 
         <div className="buttons"> 
-        <Button  onClick={handleClick1} variant="primary">Increment</Button>{' '}
+        <Button onClick={handelIncrement}  variant="primary">Increment</Button>{' '}
 
-        <Button  onClick={handleClick2}  variant="danger">Decrement</Button>{' '}
+        <Button   onClick={handeldecrement}   variant="danger">Decrement</Button>{' '}
 
         
         </div> 
